c8e1eb59670a2194acec5235d31f2a6afbc99914
barophobe <edoconnolly@gmail.com>
2018-09-10 21:04:19 +0100

    update tests completed

---
+++ prod/users/test/update_test.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+
+//set and save. Set will set something in memory but not persist it. It will only persist after save() is used.
+// Usefull if you want to make several adjustments to an oblect/document before you want ot save it.
+// update is used when you just want to do one update/make one change before saving a document
+
+//model instance updates
+
+describe('Updating records', () => {
+    let joe;
+
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe'});
+        joe.save()
+            .then(() => done());
+    });
+
+    function assertName(operation, done) {
+        operation
+            .then(() => User.find({}))
+            .then((users) => {
+                 assert(users.length === 1);
+                 assert(users[0].name === 'Alex');
+                done();
+        });
+    }
+
+    it('instance type using set and save', (done) => {
+        joe.set('name', 'Alex');
+       assertName(joe.save(), done);
+    });
+
+
+    it('A model instance can update', (done) => {
+       assertName(joe.update({ name: 'Alex' }), done)
+    });
+
+//class type updates
+
+    it('A model class can update', (done) => {
+        assertName(
+            User.update({ name: 'Joe'}, { name: 'Alex'}),
+        done
+        );
+    });
+
+    it('A model class can update one record', (done) => {
+        assertName(
+            User.findOneAndUpdate({ name: 'Joe'}, { name: 'Alex'}),
+        done
+        );
+    });
+
+    it('A model class can find a record with an Id and update', (done) => {
+        assertName(
+            User.findByIdAndUpdate( joe._id, { name: 'Alex' }),
+            done
+        );
+    });
+
+});
