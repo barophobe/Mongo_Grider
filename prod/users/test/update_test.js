decad14b4703a510171d1c8e166b7d52cac48e89
barophobe <edoconnolly@gmail.com>
2018-09-10 22:32:21 +0100

    update and validation tests completed

--- prod/users/test/update_test.js
+++ prod/users/test/update_test.js
@@ -12,7 +12,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe'});
+        joe = new User({ name: 'Joe', postCount: 0 });
         joe.save()
             .then(() => done());
     });
@@ -58,6 +58,15 @@ describe('Updating records', () => {
             User.findByIdAndUpdate( joe._id, { name: 'Alex' }),
             done
         );
+    });
+
+    it('A User can have their post count incremented by one', (done) => {
+        User.update({ name: 'Joe' }, { $inc: {postCount: 1 } })
+        .then(() => User.findOne({ name: 'Joe' }))
+        .then((user) => {
+            assert(user.postCount === 1);
+            done();
+        });
     });
 
 });
