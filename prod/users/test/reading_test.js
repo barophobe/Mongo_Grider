b790b68d17e3cbe9811c49a130db44be85aabf9f
barophobe <edoconnolly@gmail.com>
2018-09-12 17:58:00 +0100

    limit and skip, also sort

--- prod/users/test/reading_test.js
+++ prod/users/test/reading_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users from the database', () => {
-    let joe;
+    let joe, alex, maria, zac;
 
     beforeEach((done) => {
-            joe = new User({ name: 'Joe' });
-            joe.save()
+        alex = new User({ name: 'Alex' });
+        joe = new User({ name: 'Joe' });
+        maria = new User({ name: 'Maria' });
+        zac = new User({ name: 'Zac' });
+
+        Promise.all([alex.save(), joe.save(), maria.save(), zac.save()])
             .then(() => done());
         });
 
@@ -24,5 +28,19 @@ describe('Reading users from the database', () => {
             assert(user.name === 'Joe');
             done();
         });
+    });
+
+    it('can skip and limit the result set', () => {
+        // -Alex- [Joe, Maria] Zac
+        User.find({})
+        .sort({ name: 1 })
+        .skip(1)
+        .limit(2)
+        .then((users) => {
+            assert(users.length === 2 );
+            assert(users[0].name === 'Joe' );
+            assert(users[1].name === 'Maria' );
+            done();
+        })
     });
 });
